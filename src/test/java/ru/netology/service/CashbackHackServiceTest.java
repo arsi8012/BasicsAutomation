b6e8c69d833844248b9c2eef09e5b6450881a972
Oleg Atlasov <olegkui@mail.ru>
2021-10-14 16:19:52 +0400

    Change tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -12,20 +12,20 @@ public class CashbackHackServiceTest {
     public void shouldRemainIfAmountLessThen1000() {
         int executed = 100;
         int actual = cashback.remain(900);
-        assertEquals(actual, executed);
+        assertEquals(executed, actual);
     }
 
     @Test
     public void shouldRemainIfAmountMoreThen1000() {
         int executed = 900;
         int actual = cashback.remain(1100);
-        assertEquals(actual, executed);
+        assertEquals(executed, actual);
     }
 
     @Test
     public void shouldRemainIfAmountEgual1000() {
         int executed = 1000;
         int actual = cashback.remain(1000);
-        assertEquals(actual, executed);
+        assertEquals(executed, actual);
     }
 }
