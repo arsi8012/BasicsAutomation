84f8ede3f394aa16f712876922aa224ad92d02cb
Oleg Atlasov <olegkui@mail.ru>
2021-10-14 16:09:16 +0400

    Gradle and JUnit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
@@ -10,21 +10,21 @@ public class CashbackHackServiceTest {
 
     @Test
     public void shouldRemainIfAmountLessThen1000() {
-        int executed=100;
+        int executed = 100;
         int actual = cashback.remain(900);
         assertEquals(actual, executed);
     }
 
     @Test
     public void shouldRemainIfAmountMoreThen1000() {
-        int executed=900;
+        int executed = 900;
         int actual = cashback.remain(1100);
         assertEquals(actual, executed);
     }
 
     @Test
     public void shouldRemainIfAmountEgual1000() {
-        int executed=1000;
+        int executed = 1000;
         int actual = cashback.remain(1000);
         assertEquals(actual, executed);
     }
