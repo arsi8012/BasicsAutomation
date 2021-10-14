fa93fd490f0607775a517ccccc3a75eece6acce9
Oleg Atlasov <olegkui@mail.ru>
2021-10-14 15:17:11 +0400

    Gradle and JUnit5

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,31 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    CashbackHackService cashback = new CashbackHackService();
+
+    @Test
+    public void shouldRemainIfAmountLessThen1000() {
+        int executed=100;
+        int actual = cashback.remain(900);
+        assertEquals(executed,actual);
+    }
+
+    @Test
+    public void shouldRemainIfAmountMoreThen1000() {
+        int executed=900;
+        int actual = cashback.remain(1100);
+        assertEquals(executed,actual);
+    }
+
+    @Test
+    public void shouldRemainIfAmountEgual1000() {
+        int executed=1000;
+        int actual = cashback.remain(1000);
+        assertEquals(executed,actual);
+    }
+}
